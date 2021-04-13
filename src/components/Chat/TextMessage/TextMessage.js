7ab609f53f38212819db732a7d36a754c8105048
Ujjval Priyadarshi <ujjval.priyadarshi06@gmail.com>
2021-04-13 23:39:10 +0530

    first commit

---
+++ src/components/Chat/TextMessage/TextMessage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TextMessage({ users , room }) {
+    console.log(users);
+    return (
+        <div>
+            <h1>People Present in this room {room} are</h1>
+            
+        
+            
+        </div>
+    )
+}
+
+export default TextMessage;
